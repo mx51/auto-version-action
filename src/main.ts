cfc8b2ed4b40c1759754b39c7bf0338ee025d322
abomadev <aboma.kelly@gmail.com>
2022-05-06 09:23:48 +0930

    Creating get version function

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,33 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 import {wait} from './wait'
 
+/**
+ * Retrieves the package version from the package.json file
+ */
+function getVersion(path: string){
+  try {
+    const packageJson: any = require(`${path}/package.json`)
+    console.log(packageJson)
+    console.log(packageJson.version)
+    
+  } catch (error) {
+    throw new Error("File does not exist")
+  }
+
+}
+
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    console.log("HELLo WORLD")
+    console.log("Current directory:", __dirname);
+    // const ms: string = core.getInput('milliseconds')
+    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // core.debug(new Date().toTimeString())
+    // await wait(parseInt(ms, 10))
+    // core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
+    // core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
